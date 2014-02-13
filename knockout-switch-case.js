ab1646a057bed2086f8f0530e067d4c535516f61
Michael Best <mbest@dasya.com>
2014-02-12 22:58:08 -1000

    Update to work with Knockout 3.1 (but still backwards compatible)

--- knockout-switch-case.js
+++ knockout-switch-case.js
@@ -2,7 +2,7 @@
  * @license SWITCH/CASE binding for Knockout http://knockoutjs.com/
  * (c) Michael Best
  * License: MIT (http://www.opensource.org/licenses/mit-license.php)
- * Version 2.0.1
+ * Version 2.1.0
  */
 
 (function (root, factory) {
@@ -119,49 +119,63 @@ function makeCaseHandler(binding, isNot, makeValueAccessor) {
                 throw Error("case binding cannot be nested");
             // Initialize $switchIndex and push a new observable to $switchSkipNextArray
             bindingContext.$switchIndex = bindingContext.$switchSkipNextArray.push(ko.observable(false)) - 1;
-            // Call init()
-            if (ko_bindingHandlers[binding].init)
-                return ko_bindingHandlers[binding].init(element, function(){ return {}; }, allBindings, viewModel, bindingContext);
+
+            bindingContext.$caseValue = ko.observable();
+
+            ko.computed(function() {
+                var index = bindingContext.$switchIndex,
+                    isLast = (index === bindingContext.$switchSkipNextArray.length - 1),
+                    result, skipNext, noDefault;
+
+                if (index && bindingContext.$switchSkipNextArray[index-1]()) {
+                    // An earlier case binding matched: skip this one (and subsequent ones)
+                    result = false;
+                    skipNext = true;
+                } else {
+                    var value = ko_unwrap(valueAccessor());
+                    if (value === bindingContext.$else) {
+                        // If value is the special object $else, the result depends on the other case values.
+                        // If we're the last *case* item, the value must be true. $switchDefault will get
+                        // updated to *true* below, but that won't necessarily update us because it would
+                        // require a recursive update.
+                        result = bindingContext.$switchDefault() || isLast;
+                        skipNext = false;
+                    } else {
+                        // If result is true, we will skip the subsequent cases (and any default cases)
+                        noDefault = skipNext = result = checkFunction(value, bindingContext);
+                    }
+                }
+
+                // Set the observable used by the wrapped binding function
+                bindingContext.$caseValue(result);
+
+                // Update the observable "skip next" value; if the value is changed, this will update the
+                // subsequent case item.
+                bindingContext.$switchSkipNextArray[index](skipNext);
+
+                // Update $switchDefault to false if a non-default case item has matched.
+                // Update it to true if we're the last item and none of items have matched.
+                // (Initially, every item will be the last, but it doesn't matter.)
+                if (noDefault)
+                    bindingContext.$switchDefault(false);
+                else if (!skipNext && isLast)
+                    bindingContext.$switchDefault(true);
+            },
+            null, { disposeWhenNodeIsRemoved: element });
+
+            // Call init with the observable result value
+            if (ko_bindingHandlers[binding].init) {
+                return ko_bindingHandlers[binding].init(element,
+                    makeValueAccessor(bindingContext.$caseValue), allBindings, viewModel, bindingContext);
+            }
         },
 
         update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
-            var index = bindingContext.$switchIndex,
-                isLast = (index === bindingContext.$switchSkipNextArray.length - 1),
-                result, skipNext, noDefault;
-
-            if (index && bindingContext.$switchSkipNextArray[index-1]()) {
-                // An earlier case binding matched: skip this one (and subsequent ones)
-                result = false;
-                skipNext = true;
-            } else {
-                var value = ko_unwrap(valueAccessor());
-                if (value === bindingContext.$else) {
-                    // If value is the special object $else, the result depends on the other case values.
-                    // If we're the last *case* item, the value must be true. $switchDefault will get
-                    // updated to *true* below, but that won't necessarily update us because it would
-                    // require a recursive update.
-                    result = bindingContext.$switchDefault() || isLast;
-                    skipNext = false;
-                } else {
-                    // If result is true, we will skip the subsequent cases (and any default cases)
-                    noDefault = skipNext = result = checkFunction(value, bindingContext);
-                }
+            // Call update with the observable result value
+            if (ko_bindingHandlers[binding].update) {
+                return ko_bindingHandlers[binding].update(element,
+                    makeValueAccessor(bindingContext.$caseValue), allBindings, viewModel, bindingContext);
             }
-            // Call update() with calculated value
-            ko_bindingHandlers[binding].update(element,
-                makeValueAccessor(result), allBindings, viewModel, bindingContext);
-
-            // Update the observable "skip next" value; if the value is changed, this will update the
-            // subsequent case item.
-            bindingContext.$switchSkipNextArray[index](skipNext);
-
-            // Update $switchDefault to false if a non-default case item has matched.
-            // Update it to true if we're the last item and none of items have matched.
-            // (Initially, every item will be the last, but it doesn't matter.)
-            if (noDefault)
-                bindingContext.$switchDefault(false);
-            else if (!skipNext && isLast)
-                bindingContext.$switchDefault(true);
         }
     };
 }
